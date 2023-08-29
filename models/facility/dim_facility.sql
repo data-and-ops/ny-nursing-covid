ff3d1e25a280b01e0757b83a8117117683f305bf
Greg Meyer - Data & Ops <greg@datanops.com>
2023-08-29 17:48:06 +0000

    missed a save

--- models/facility/dim_facility.sql
+++ models/facility/dim_facility.sql
@@ -3,7 +3,7 @@ with base_facility as (
 ),
 
 final as (
-  -- produce a summary list by facility
+  -- produce a summary list by facility, sorted by total deaths desc
   select 
     federal_provider_number,
     provider_name,
@@ -13,8 +13,8 @@ final as (
     max(residents_total_covid19_deaths) as residents_total_covid19_deaths
  
   from base_facility f 
-  group by federal_provider_number, provider_name, provider_city, county
-  order by residents_total_covid19_deaths desc 
+  group by county, provider_city, provider_name, federal_provider_number
+  order by county asc, residents_total_covid19_deaths desc 
 )
 
 select * from final
